98a475d236c4fb616d18e6315ca9dfd648a83f45
Josiel <jdlopez114@gmail.com>
2023-02-13 20:40:18 -0500

    commit tree 06df7df126963d03097b106bde1cd5a4b80cf8ea

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0 
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist 
+        @genre = genre 
+
+        @@count +=1 
+        @@genres << genre
+        @@artists << artist 
+    end
+
+    def self.count 
+        return @@count 
+    end
+
+    def self.genres
+        return @@genres.uniq 
+    end
+
+    def self.artists
+        return @@artists.uniq 
+    end
+
+    def self.genre_count
+        return @@genres.tally 
+    end
+
+    def self.artist_count
+        return @@artists.tally 
+    end
+
+end
